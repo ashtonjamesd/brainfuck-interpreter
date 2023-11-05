4b491b16ca6e8d3d57c3b194a69dc176c31fe6f6
Ashton Dunderdale <121163457+ashtondunderdale@users.noreply.github.com>
2023-11-05 18:14:08 +0000

    added brainfuck

--- brainfuck/Program.cs
+++ brainfuck/Program.cs
@@ -1,10 +1,69 @@
-﻿namespace brainfuck
+﻿
+/// <summary>
+/// A simple interpreter for the Brainfuck language
+/// </summary>
+class BrainFuckInterpreter
 {
-    internal class Program
+    private const int TAPE_SIZE = 32767;
+
+    static void Main()
     {
-        static void Main(string[] args)
+        string code = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
+        BrainFuck(code);
+    }
+
+    static void BrainFuck(string code)
+    {
+        int instructionPointer = 0; int dataPointer = 0;
+        byte[] tape = new byte[TAPE_SIZE]; Stack<int> loopStack = new();
+
+        while (instructionPointer < code.Length)
         {
-            Console.WriteLine("Hello, World!");
+            char command = code[instructionPointer];
+
+            switch (command)
+            {
+                case '>': dataPointer++; instructionPointer++; break;
+                case '<': dataPointer--; instructionPointer++; break;
+
+                case '+': tape[dataPointer]++; instructionPointer++; break;
+                case '-': tape[dataPointer]--; instructionPointer++; break;
+
+                case '.': Console.Write(Convert.ToChar(tape[dataPointer]));  instructionPointer++; break;
+
+                case ',':
+                    int input = Console.Read();
+
+                    if (input == 13)
+                    {
+                        instructionPointer++;
+                        continue;
+                    }
+
+                    tape[dataPointer] = (byte)input;
+                    break;
+
+                case '[':
+                    if (tape[dataPointer] == 0)
+                    {
+                        int depth = 1;
+                        while (depth > 0)
+                        {
+                            instructionPointer++;
+                            if (instructionPointer >= code.Length) break;
+
+                            if (code[instructionPointer] == '[') depth++;
+                            else if (code[instructionPointer] == ']') depth--;
+                        }
+                    }
+                    else loopStack.Push(instructionPointer); instructionPointer++; break;
+
+                case ']':
+                    if (tape[dataPointer] != 0) instructionPointer = loopStack.Peek() - 1;
+                    else loopStack.Pop(); instructionPointer++; break;
+
+                default: break;
+            }
         }
     }
 }
